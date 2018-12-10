9024f9bf95f3a24174b0853c918d6be5051ff350
Phodal HUANG <h@phodal.com>
2018-12-10 13:03:23 +0800

    [T] tink in layout

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 function init() {
   var stage = new createjs.Stage("demoCanvas");
   var circle = new createjs.Shape();
-  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+
+  var text = new createjs.Text("Hello World", "20px Arial", "#ff7700");
+  text.x = 100;
+  text.y = 100;
+  text.textBaseline = "alphabetic";
+
+  circle.graphics.beginFill("#000").drawCircle(0, 0, 50);
   circle.x = 100;
   circle.y = 100;
   stage.addChild(circle);
+  stage.update();
+  stage.addChild(text);
   stage.update();
 }
