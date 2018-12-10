c69647bd9b7980883bef807decad9d7bfb532bdf
Phodal HUANG <h@phodal.com>
2018-12-10 12:45:22 +0800

    feat: init repos

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function init() {
+  var stage = new createjs.Stage("demoCanvas");
+  var circle = new createjs.Shape();
+  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+  circle.x = 100;
+  circle.y = 100;
+  stage.addChild(circle);
+  stage.update();
+}
