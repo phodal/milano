dbfb5f019ea8d1c7cd4a51072dd1a98b5172f8c0
Phodal HUANG <h@phodal.com>
2018-12-12 07:53:27 +0800

    effec: init snowfalling

---
+++ src/effects/snow.js
@@ -0,0 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Andy+
+ *
+ * GitHub: https://github.com/zzlw/createjs
+ *
+ */
+define([
+  'createjs'
+], function () {
+  function Snow(stage) {
+
+  }
+
+  Snow.prototype.init = function () {
+
+  };
+
+  Snow.prototype.action = function () {
+
+  };
+
+  Snow.prototype.finish = function () {
+
+  };
+
+  return Snow;
+});
