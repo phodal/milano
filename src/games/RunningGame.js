dca49debe3484ffb2f6c096f66b9b81d8f2c57e3
Phodal HUANG <h@phodal.com>
2018-12-24 17:07:13 +0800

    feat: [game] thinking in intersection

--- src/games/RunningGame.js
+++ src/games/RunningGame.js
@@ -145,6 +145,11 @@ define([
     }
   };
 
+  function checkIntersection(rect1, rect2) {
+    if (rect1.x >= rect2.x + rect2.width || rect1.x + rect1.width <= rect2.x || rect1.y >= rect2.y + rect2.height || rect1.y + rect1.height <= rect2.y) return false;
+    return true;
+  }
+
   RunningGame.prototype.action = function () {
     return sceneContainer;
   };
