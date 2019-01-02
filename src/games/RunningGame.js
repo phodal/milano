1e3524a30284b81d52eecf0822d86dcff30c938a
Phodal HUANG <h@phodal.com>
2019-01-02 17:45:18 +0800

    refactor: fix styles

--- src/games/RunningGame.js
+++ src/games/RunningGame.js
@@ -146,7 +146,12 @@ define([
   };
 
   function checkIntersection(rect1, rect2) {
-    if (rect1.x >= rect2.x + rect2.width || rect1.x + rect1.width <= rect2.x || rect1.y >= rect2.y + rect2.height || rect1.y + rect1.height <= rect2.y) return false;
+    if (rect1.x >= rect2.x + rect2.width
+      || rect1.x + rect1.width <= rect2.x
+      || rect1.y >= rect2.y + rect2.height
+      || rect1.y + rect1.height <= rect2.y) {
+      return false;
+    }
     return true;
   }
 
